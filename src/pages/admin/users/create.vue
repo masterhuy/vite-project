8b8973b1c1fc470a526f6efd8680a61e69b1139b
Quang Huy <nqh95014@gmail.com>
2024-05-08 17:25:29 +0700

    #21 validation

--- src/pages/admin/users/create.vue
+++ src/pages/admin/users/create.vue
@@ -1,166 +1,176 @@
 <template>
-    <a-card title="Tạo mới Tài khoản" style="width: 100%">
-      <div class="row mb-3">
-        <div class="col-12 col-sm-4 mb-3">
-          <div class="row">
-            <div class="col-12 d-flex justify-content-center mb-3">
-              <a-avatar shape="square" :size="200">
-                <template #icon>
-                  <img src="../../../assets/users.jpg" alt="Avatar" />
-                </template>
-              </a-avatar>
+    <form @submit.prevent="createUsers()">
+        <a-card title="Tạo mới Tài khoản" style="width: 100%">
+        <div class="row mb-3">
+            <div class="col-12 col-sm-4 mb-3">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center mb-3">
+                <a-avatar shape="square" :size="200">
+                    <template #icon>
+                    <img src="../../../assets/users.jpg" alt="Avatar" />
+                    </template>
+                </a-avatar>
+                </div>
+    
+                <div class="col-12 d-flex justify-content-center">
+                <a-button type="primary">
+                    <i class="fa-solid fa-plus me-2"></i>
+                    <span>Chọn ảnh</span>
+                </a-button>
+                </div>
             </div>
-  
-            <div class="col-12 d-flex justify-content-center">
-              <a-button type="primary">
-                <i class="fa-solid fa-plus me-2"></i>
-                <span>Chọn ảnh</span>
-              </a-button>
             </div>
-          </div>
-        </div>
-  
-        <div class="col-12 col-sm-8">
+    
+            <div class="col-12 col-sm-8">
+                <div class="row mb-3">
+            <div class="col-12 col-sm-3 text-start text-sm-end">
+                <label>
+                <span class="text-danger me-1">*</span>
+                <span>Tình trạng:</span>
+                </label>
+            </div>
+
+            <div class="col-12 col-sm-5">
+                <a-select
+                show-search
+                placeholder="Tình trạng"
+                style="width: 100%"
+                :options="users_status"
+                :filter-option="filterOption"
+                allow-clear
+                v-model:value="status_id"
+                ></a-select>
+            </div>
+            </div>
+
             <div class="row mb-3">
-          <div class="col-12 col-sm-3 text-start text-sm-end">
-            <label>
-              <span class="text-danger me-1">*</span>
-              <span>Tình trạng:</span>
-            </label>
-          </div>
+            <div class="col-12 col-sm-3 text-start text-sm-end">
+                <label>
+                <span class="text-danger me-1">*</span>
+                <span>Tên Tài khoản:</span>
+                </label>
+            </div>
 
-          <div class="col-12 col-sm-5">
-            <a-select
-              show-search
-              placeholder="Tình trạng"
-              style="width: 100%"
-              :options="users_status"
-              :filter-option="filterOption"
-              allow-clear
-            ></a-select>
-          </div>
+            <div class="col-12 col-sm-5">
+                <a-input
+                placeholder="Tên Tài khoản"
+                allow-clear
+                v-model:value="username"
+                />
+            </div>
+            </div>
+
+            <div class="row mb-3">
+            <div class="col-12 col-sm-3 text-start text-sm-end">
+                <label>
+                <span class="text-danger me-1">*</span>
+                <span>Họ và Tên:</span>
+                </label>
+            </div>
+
+            <div class="col-12 col-sm-5">
+                <a-input
+                placeholder="Họ và Tên"
+                allow-clear
+                v-model:value="name"
+                />
+            </div>
+            </div>
+
+            <div class="row mb-3">
+            <div class="col-12 col-sm-3 text-start text-sm-end">
+                <label>
+                <span class="text-danger me-1">*</span>
+                <span>Email:</span>
+                </label>
+            </div>
+
+            <div class="col-12 col-sm-5">
+                <a-input
+                placeholder="Email"
+                allow-clear
+                v-model:value="email"
+                />
+            </div>
+            </div>
+
+            <div class="row mb-3">
+            <div class="col-12 col-sm-3 text-start text-sm-end">
+                <label>
+                <span class="text-danger me-1">*</span>
+                <span>Phòng ban:</span>
+                </label>
+            </div>
+
+            <div class="col-12 col-sm-5">
+                <a-select
+                show-search
+                placeholder="Phòng ban"
+                style="width: 100%"
+                :options="departments"
+                :filter-option="filterOption"
+                v-model:value="departments_id"
+                ></a-select>
+            </div>
+            </div>
+
+            <div class="row mb-3">
+            <div class="col-12 col-sm-3 text-start text-sm-end">
+                <label>
+                <span class="text-danger me-1">*</span>
+                <span>Mật khẩu:</span>
+                </label>
+            </div>
+
+            <div class="col-12 col-sm-5">
+                <a-input-password
+                placeholder="Mật khẩu"
+                allow-clear
+                v-model:value="password"
+                />
+            </div>
+            </div>
+
+            <div class="row mb-3">
+            <div class="col-12 col-sm-3 text-start text-sm-end">
+                <label>
+                <span class="text-danger me-1">*</span>
+                <span>Xác nhận mật khẩu:</span>
+                </label>
+            </div>
+
+            <div class="col-12 col-sm-5">
+                <a-input-password
+                placeholder="Xác nhận mật khẩu"
+                allow-clear
+                v-model:value="password_confirmation"
+                />
+            </div>
+            </div>
+            </div>
         </div>
 
-        <div class="row mb-3">
-          <div class="col-12 col-sm-3 text-start text-sm-end">
-            <label>
-              <span class="text-danger me-1">*</span>
-              <span>Tên Tài khoản:</span>
-            </label>
-          </div>
+        <div class="row">
+            <div class="col-12 d-grid d-sm-flex justify-content-sm-end mx-auto">
+                <a-button class="me-0 me-sm-2 mb-3 mb-sm-0">
+                <router-link :to="{ name: 'admin-users' }">
+                    <span>Hủy</span>
+                </router-link>
+                </a-button>
 
-          <div class="col-12 col-sm-5">
-            <a-input
-              placeholder="Tên Tài khoản"
-              allow-clear
-            />
-          </div>
-        </div>
-
-        <div class="row mb-3">
-          <div class="col-12 col-sm-3 text-start text-sm-end">
-            <label>
-              <span class="text-danger me-1">*</span>
-              <span>Họ và Tên:</span>
-            </label>
-          </div>
-
-          <div class="col-12 col-sm-5">
-            <a-input
-              placeholder="Họ và Tên"
-              allow-clear
-            />
-          </div>
-        </div>
-
-        <div class="row mb-3">
-          <div class="col-12 col-sm-3 text-start text-sm-end">
-            <label>
-              <span class="text-danger me-1">*</span>
-              <span>Email:</span>
-            </label>
-          </div>
-
-          <div class="col-12 col-sm-5">
-            <a-input
-              placeholder="Email"
-              allow-clear
-            />
-          </div>
-        </div>
-
-        <div class="row mb-3">
-          <div class="col-12 col-sm-3 text-start text-sm-end">
-            <label>
-              <span class="text-danger me-1">*</span>
-              <span>Phòng ban:</span>
-            </label>
-          </div>
-
-          <div class="col-12 col-sm-5">
-            <a-select
-              show-search
-              placeholder="Phòng ban"
-              style="width: 100%"
-              :options="departments"
-              :filter-option="filterOption"
-            ></a-select>
-          </div>
-        </div>
-
-        <div class="row mb-3">
-          <div class="col-12 col-sm-3 text-start text-sm-end">
-            <label>
-              <span class="text-danger me-1">*</span>
-              <span>Mật khẩu:</span>
-            </label>
-          </div>
-
-          <div class="col-12 col-sm-5">
-            <a-input-password
-              placeholder="Mật khẩu"
-              allow-clear
-            />
-          </div>
-        </div>
-
-        <div class="row mb-3">
-          <div class="col-12 col-sm-3 text-start text-sm-end">
-            <label>
-              <span class="text-danger me-1">*</span>
-              <span>Xác nhận mật khẩu:</span>
-            </label>
-          </div>
-
-          <div class="col-12 col-sm-5">
-            <a-input-password
-              placeholder="Xác nhận mật khẩu"
-              allow-clear
-            />
-          </div>
-        </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-12 d-grid d-sm-flex justify-content-sm-end mx-auto">
-            <a-button class="me-0 me-sm-2 mb-3 mb-sm-0">
-            <router-link :to="{ name: 'admin-users' }">
-                <span>Hủy</span>
-            </router-link>
-            </a-button>
-
-            <a-button type="primary">
-            <span>Lưu</span>
-            </a-button>
-        </div>
-        </div>
-    </a-card>
+                <a-button type="primary" html-type="submit">
+                <span>Lưu</span>
+                </a-button>
+            </div>
+            </div>
+        </a-card>
+    </form>
   </template>
   
 <script>
-    import { defineComponent, ref } from "vue";
+    import { defineComponent, ref, reactive, toRefs } from "vue";
     import { useMenu } from "../../../stores/use-menu.js";
+import axios from "axios";
     
     export default defineComponent({
         setup() {
@@ -168,6 +178,14 @@
 
             const users_status = ref([])
             const departments = ref([])
+            const users = reactive({
+                username: "",
+                name: "",
+                password: "",
+                password_confirmation: "",
+                department_id: [],
+                status_id: []
+            })
 
             const getUserCreate = () => {
                 axios.get('http://127.0.0.1:8000/api/users/create')
@@ -182,6 +200,16 @@
                     });
             }
 
+            const createUsers = () =>{
+                axios.post("http://127.0.0.1:8000/api/users", users)
+                .then((response) => {
+                    console.log(response)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            }
+
             getUserCreate()
 
             const filterOption = (input, option) => {
@@ -192,7 +220,9 @@
             return {
                 users_status,
                 departments,
-                filterOption
+                filterOption,
+                createUsers,
+                ...toRefs(users)
             }
         },
     });
